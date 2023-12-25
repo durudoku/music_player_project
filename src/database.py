859dff8b2e155d76d3b0473116ce28b51a1c289a
Duru <duru.dokuzluoglu@gmail.com>
2023-12-25 18:38:46 +0300

    created playlist and song databases and added their lists to the main page

--- src/database.py
+++ src/database.py
@@ -1,8 +1,59 @@
 import sqlite3
 
 class Database:
+    def __init__(self):
+        self.user_conn = sqlite3.connect("users.db")
+        self.playlist_conn = sqlite3.connect("playlist.db")
+        self.song_conn = sqlite3.connect("song.db")
+        self.conn = sqlite3.connect("music.db")
+
+    def create_tables(self):
+        self.create_table_users()
+        self.create_table_playlists()
+        self.create_table_songs()
+
+    # Playlist Operations
+    def create_table_playlists(self):
+        connection = self.playlist_conn
+        cursor = connection.cursor()
+        cursor.execute("CREATE TABLE IF NOT EXISTS playlists (id INTEGER PRIMARY KEY, name TEXT)")
+        connection.commit()
+
+    def add_playlist(self, playlist_name):
+        connection = self.playlist_conn
+        cursor = connection.cursor()
+        cursor.execute("INSERT INTO playlists (name) VALUES (?)", (playlist_name,))
+        connection.commit()
+
+    def fetch_playlists(self):
+        connection = self.playlist_conn
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM playlists")
+        playlists = cursor.fetchall()
+        return playlists
+
+    # Song Operations
+    def create_table_songs(self):
+        connection = self.song_conn
+        cursor = connection.cursor()
+        cursor.execute("CREATE TABLE IF NOT EXISTS songs (id INTEGER PRIMARY KEY, name TEXT, artist TEXT, playlist TEXT)")
+        connection.commit()
+
+    def create_table_playlist_songs(self):
+        cursor = self.conn.cursor()
+        cursor.execute(
+            "CREATE TABLE IF NOT EXISTS playlist_songs (playlist_id INTEGER, song_id INTEGER, FOREIGN KEY(playlist_id) REFERENCES playlists(id), FOREIGN KEY(song_id) REFERENCES songs(id), PRIMARY KEY(playlist_id, song_id))")
+        self.conn.commit()
+
+    def fetch_songs_for_playlist(self, playlist_name):
+        connection = self.song_conn
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM songs WHERE playlist=?", (playlist_name,))
+        songs = cursor.fetchall()
+        return songs
+
     @staticmethod
-    def create_table():
+    def create_table_users():
         connection = sqlite3.connect("users.db")
         cursor = connection.cursor()
         cursor.execute(
@@ -38,4 +89,5 @@ class Database:
         return user is not None
 
 if __name__ == "__main__":
-    Database.create_table()
+    db_instance = Database()
+    db_instance.create_tables()
--- src/main_page.py
+++ src/main_page.py
@@ -1,13 +1,63 @@
 import tkinter as tk
+from tkinter import scrolledtext
+from database import Database
 
 class MainPageGUI:
     def __init__(self, root):
         self.root = root
         self.root.title("Main Page")
-        self.root.geometry("250x300+100+100")
 
-        label = tk.Label(root, text="Welcome to the Main Page!")
-        label.pack()
+        # Initialize database operations
+        self.db = Database()
+
+        # Create GUI elements
+        self.playlist_listbox = scrolledtext.ScrolledText(root, width=30, height=10, state=tk.DISABLED)
+        self.playlist_listbox.pack(side=tk.LEFT, padx=10, pady=10)
+
+        self.song_listbox = scrolledtext.ScrolledText(root, width=40, height=10, state=tk.DISABLED)
+        self.song_listbox.pack(side=tk.LEFT, padx=10, pady=10)
+
+        # Populate playlists
+        self.populate_playlists()
+
+        # Set event handler for playlist selection
+        self.playlist_listbox.bind("<ButtonRelease-1>", self.show_selected_playlist_songs)
+
+    def populate_playlists(self):
+        # Fetch playlists from the database
+        playlists = self.db.fetch_playlists()
+
+        # Display playlists in the listbox
+        for playlist in playlists:
+            self.playlist_listbox.insert(tk.END, playlist[1] + "\n")
+
+    def show_selected_playlist_songs(self, event):
+        # Clear existing songs in the song listbox
+        self.song_listbox.config(state=tk.NORMAL)
+        self.song_listbox.delete(1.0, tk.END)
+        self.song_listbox.config(state=tk.DISABLED)
+
+        # Get the indices of the selected text
+        sel_start_index = self.playlist_listbox.tag_ranges(tk.SEL + ' first')
+        sel_end_index = self.playlist_listbox.tag_ranges(tk.SEL + ' last')
+
+        # Ensure there is a selection
+        if sel_start_index and sel_end_index:
+            # Get the selected text using the indices
+            sel_start = self.playlist_listbox.index(sel_start_index)
+            sel_end = self.playlist_listbox.index(sel_end_index)
+
+            selected_playlist = self.playlist_listbox.get(sel_start, sel_end).strip()
+
+            # Fetch songs for the selected playlist from the database
+            songs = self.db.fetch_songs_for_playlist(selected_playlist)
+
+            # Display songs in the listbox
+            self.song_listbox.config(state=tk.NORMAL)
+            for song in songs:
+                self.song_listbox.insert(tk.END, f"{song[1]} by {song[2]}\n")
+            self.song_listbox.config(state=tk.DISABLED)
+
 
 if __name__ == "__main__":
     root = tk.Tk()
