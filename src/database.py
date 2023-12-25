1e3abcfc179f4fc6208df740a6f327ca1fb9bc45
Duru <duru.dokuzluoglu@gmail.com>
2023-12-26 00:56:41 +0300

    playlist treeview eklendi, add ve remove buttonları çalışır hale getirildi

--- src/database.py
+++ src/database.py
@@ -16,14 +16,19 @@ class Database:
     def create_table_playlists(self):
         connection = self.playlist_conn
         cursor = connection.cursor()
-        cursor.execute("CREATE TABLE IF NOT EXISTS playlists (id INTEGER PRIMARY KEY, name TEXT)")
+        cursor.execute("CREATE TABLE IF NOT EXISTS playlists (id INTEGER PRIMARY KEY, name TEXT, songs TEXT)")
         connection.commit()
 
-    def add_playlist(self, playlist_name):
-        connection = self.playlist_conn
-        cursor = connection.cursor()
-        cursor.execute("INSERT INTO playlists (name) VALUES (?)", (playlist_name,))
-        connection.commit()
+    def add_playlist(self, name, songs=None):
+        cursor = self.playlist_conn.cursor()
+        songs = songs if songs is not None else ""
+        cursor.execute("INSERT INTO playlists (name, songs) VALUES (?, ?)", (name, songs))
+        self.playlist_conn.commit()
+
+    def remove_playlist(self, playlist_id):
+        cursor = self.playlist_conn.cursor()
+        cursor.execute("DELETE FROM playlists WHERE id=?", (playlist_id,))
+        self.playlist_conn.commit()
 
     def fetch_playlists(self):
         connection = self.playlist_conn
@@ -45,12 +50,14 @@ class Database:
             "CREATE TABLE IF NOT EXISTS playlist_songs (playlist_id INTEGER, song_id INTEGER, FOREIGN KEY(playlist_id) REFERENCES playlists(id), FOREIGN KEY(song_id) REFERENCES songs(id), PRIMARY KEY(playlist_id, song_id))")
         self.conn.commit()
 
-    def fetch_songs_for_playlist(self, playlist_name):
-        connection = self.song_conn
-        cursor = connection.cursor()
-        cursor.execute("SELECT * FROM songs WHERE playlist=?", (playlist_name,))
-        songs = cursor.fetchall()
-        return songs
+    def fetch_songs_for_playlist_id(self, playlist_id):
+        cursor = self.playlist_conn.cursor()
+        cursor.execute("SELECT * FROM playlists WHERE id=?", (playlist_id,))
+        playlist = cursor.fetchone()
+        if playlist:
+            return playlist[2]  # Assuming songs are stored in the third column (index 2)
+        else:
+            return None
 
     @staticmethod
     def create_table_users():
--- src/main_page.py
+++ src/main_page.py
@@ -9,19 +9,28 @@ class MainPageGUI:
         self.root.title("Main Page")
         self.root.geometry("750x300+100+100")
 
+        # Initialize database operations
         self.db = Database()
 
         # GUI Elements
         self.label_playlists = tk.Label(root, text="Playlists")
         self.label_playlists.place(x=10, y=30)
 
-        self.playlist_listbox = scrolledtext.ScrolledText(root, width=30, height=10, state=tk.DISABLED)
-        self.playlist_listbox.place(x=10, y=50)
+        # Create Treeview for playlists
+        self.playlist_tree = ttk.Treeview(root, columns=("ID", "Name"), show="headings", selectmode="browse")
+        self.playlist_tree.heading("ID", text="ID")
+        self.playlist_tree.heading("Name", text="Name")
+        self.playlist_tree.column("ID", width=50)
+        self.playlist_tree.column("Name", width=200)
+        self.playlist_tree.place(x=10, y=50)
+
+        # Bind treeview selection event
+        self.playlist_tree.bind("<ButtonRelease-1>", self.show_selected_playlist_songs)
 
         self.button_playlist_add = ttk.Button(root, text="+", width=5, command=self.show_add_playlist_gui)
         self.button_playlist_add.place(x=270, y=65)
 
-        self.button_playlist_remove = ttk.Button(root, text="-", width=5)
+        self.button_playlist_remove = ttk.Button(root, text="-", width=5, command=self.remove_selected_playlist)
         self.button_playlist_remove.place(x=270, y=170)
 
         self.label_songs = tk.Label(root, text="Songs")
@@ -36,50 +45,38 @@ class MainPageGUI:
         self.button_song_remove = ttk.Button(root, text="-", width=5)
         self.button_song_remove.place(x=670, y=170)
 
+        # Populate playlists
         self.populate_playlists()
 
-        self.playlist_listbox.bind("<ButtonRelease-1>", self.show_selected_playlist_songs)
-
     def populate_playlists(self):
-        # Clear existing content in the playlist listbox
-        self.playlist_listbox.config(state=tk.NORMAL)
-        self.playlist_listbox.delete(1.0, tk.END)
+        # Clear existing content in the playlist treeview
+        self.playlist_tree.delete(*self.playlist_tree.get_children())
 
         # Fetch playlists from the database
         playlists = self.db.fetch_playlists()
 
-        # Display playlists in the listbox
+        # Display playlists in the treeview
         for playlist in playlists:
-            self.playlist_listbox.insert(tk.END, playlist[1] + "\n")
-
-        # Disable the playlist listbox for read-only
-        self.playlist_listbox.config(state=tk.DISABLED)
+            self.playlist_tree.insert("", "end", values=(playlist[0], playlist[1]))
 
     def show_selected_playlist_songs(self, event):
-        # Clear existing songs in the song listbox
-        self.song_listbox.config(state=tk.NORMAL)
-        self.song_listbox.delete(1.0, tk.END)
-        self.song_listbox.config(state=tk.DISABLED)
+        # Get the selected item in the treeview
+        selected_item = self.playlist_tree.selection()
 
-        # Get the indices of the selected text
-        sel_start_index = self.playlist_listbox.tag_ranges(tk.SEL + ' first')
-        sel_end_index = self.playlist_listbox.tag_ranges(tk.SEL + ' last')
-
-        # Ensure there is a selection
-        if sel_start_index and sel_end_index:
-            # Get the selected text using the indices
-            sel_start = self.playlist_listbox.index(sel_start_index)
-            sel_end = self.playlist_listbox.index(sel_end_index)
-
-            selected_playlist = self.playlist_listbox.get(sel_start, sel_end).strip()
+        if selected_item:
+            # Get the playlist ID from the selected item
+            playlist_id = self.playlist_tree.item(selected_item, "values")[0]
 
             # Fetch songs for the selected playlist from the database
-            songs = self.db.fetch_songs_for_playlist(selected_playlist)
+            songs = self.db.fetch_songs_for_playlist_id(playlist_id)
 
             # Display songs in the listbox
             self.song_listbox.config(state=tk.NORMAL)
-            for song in songs:
-                self.song_listbox.insert(tk.END, f"{song[1]} by {song[2]}\n")
+            self.song_listbox.delete(1.0, tk.END)
+            # Display songs in the listbox
+            if songs:
+                for song in songs.split(','):
+                    self.song_listbox.insert(tk.END, f"{song}\n")
             self.song_listbox.config(state=tk.DISABLED)
 
     def show_add_playlist_gui(self):
@@ -92,6 +89,20 @@ class MainPageGUI:
 
     def show_add_song_gui(self):
         pass
+
+    def remove_selected_playlist(self):
+        # Get the selected item in the treeview
+        selected_item = self.playlist_tree.selection()
+
+        if selected_item:
+            # Get the playlist ID from the selected item
+            playlist_id = self.playlist_tree.item(selected_item, "values")[0]
+
+            # Remove the playlist from the database
+            self.db.remove_playlist(playlist_id)
+
+            # Update the playlist treeview
+            self.populate_playlists()
 
 
 if __name__ == "__main__":
